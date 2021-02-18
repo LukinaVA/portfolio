331076b9fe192a56a47cfbf41e3c250697c236a6
LukinaVA <lukina.va@edu.spbstu.ru>
2021-02-18 13:25:44 +0300

    Works

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import Btn from '../Common/Btn';
 import Container from '../Common/Container';
 import './style.scss'
+import MoGo from './MoGo.jpg'
+import MNTN from './MNTN.jpg'
 
 export default function Works() {
     return (
         <div className='works'>
             <Container>
+                <h2 className='works__title'>Latest Works</h2>
+                <div className='works__content'>
+                    <div className='works__card card'>
+                        <div className='card__bg'>
+                            <img src={MoGo}/>
+                        </div>
+                        <div className='card__content'>
+                            <h3 className='card__name'>MoGo</h3>
+                            <Btn href='#' text='More Details'/>
+                        </div>
+                    </div>
 
+                    <div className='works__card card'>
+                        <div className='card__bg'>
+                            <img src={MNTN}/>
+                        </div>
+                        <div className='card__content'>
+                            <h3 className='card__name'>MNTN</h3>
+                            <Btn href='#' text='More Details'/>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </div>
     );
