ba795aa55ca284d2d68500f4980958afda8ef086
LukinaVA <lukina.va@edu.spbstu.ru>
2021-03-04 17:03:04 +0300

    Adaptive 1920

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
         <footer className='footer'>
             <Container>
                 <div className='footer__networks'>
-                    <a href='https://github.com/LukinaVA' className='footer__link'>Github</a>
-                    <a href='https://vk.com/lukinaaaaaaa' className='footer__link'>VK</a>
-                    <a href='https://www.instagram.com/lukinaaa_a/' className='footer__link'>Instagram</a>
+                    <a href='https://github.com/LukinaVA' className='footer__link' target='_blank' rel='noreferrer'>Github</a>
+                    <a href='https://vk.com/lukinaaaaaaa' className='footer__link' target='_blank' rel='noreferrer'>VK</a>
+                    <a href='https://www.instagram.com/lukinaaa_a/' className='footer__link' target='_blank' rel='noreferrer'>Instagram</a>
                 </div>
                 <div className='footer__contact'>Telegram: <span>@LukinaVA</span></div>
             </Container>
